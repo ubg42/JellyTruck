472c935b1c6b66471c8531a8c9ec57d8179a27e0
ubg42 <redtomatotiger@gmail.com>
2026-06-20 15:56:12 +1000

    Create screen.js

---
+++ js/screen.js
@@ -0,0 +1,11 @@
+(function () {
+    document.querySelectorAll("canvas").forEach(canvas => {
+        if (canvas.requestFullscreen) {
+            canvas.requestFullscreen();
+        } else if (canvas.webkitRequestFullscreen) {
+            canvas.webkitRequestFullscreen();
+        } else if (canvas.msRequestFullscreen) {
+            canvas.msRequestFullscreen();
+        }
+    });
+})();
